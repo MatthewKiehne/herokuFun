bfd14cc0ae1b12662781fcc3144ba5ddb6e1907f
MatthewKiehne <mc.lanesboro@gmail.com>
2021-04-15 20:06:23 -0500

    backend calling an api

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch');
+
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -10,12 +12,14 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => { 
-  res.render('pages/index') 
+app.get('/', (req, res) => {
+  res.render('pages/index')
 });
 
-app.get('/api', (req, res) => {
+app.get('/api', async (req, res) => {
   res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({a: 1}, null, 3));
+  const test = await fetch('https://cat-fact.herokuapp.com/facts')
+  const facts = await test.json();
+  res.end(JSON.stringify(facts));
 });
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
